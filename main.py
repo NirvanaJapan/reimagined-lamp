bbd7d2d98a772035250254ab404de8042ec45564
shinji.ichihara <shinji.ichihara@enish.com>
2019-07-10 10:33:22 +0000

    wip

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import requests
-response = requests.get("http://www.shugiin.go.jp/internet/itdb_annai.nsf/html/statics/syu/1giin.htm")
-print (response.text)
-html_doc = requests.get("https://review-of-my-life.blogspot.com").text
-soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
-print(soup.prettify())
+from bs4 import BeautifulSoup
+import re
+
+url = "http://www.shugiin.go.jp/internet/itdb_annai.nsf/html/statics/syu/1giin.htm"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')  # BeautifulSoupの初期化
+
+profile = []
+table = soup.findAll("table")[2]
+tds = table.findAll("tr")
+tds.pop(0)
+for td in tds:
+    sh1td5 = td.find("td", {"class": "sh1td5"})
+    n = sh1td5.find("a")
+    nametext = n.text
+    link = n.get("href")
+    sepa =
+    print(nametext + " " + link)
+
+#names = soup.findAll("td", {"class": "sh1td5"})
+# for n in names:
+#    print(n.text)
+#shtd = tbody.findAll("td", {"class": "sh1td5"})
+# print(shtd)
